8a301d19afab6d59615dc0b868c7b5247c523fc5
Khafidhfuadi <muhammadkhafidfuadi@gmail.com>
2020-09-07 10:39:22 +0700

    messegeCommit

---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/category', function () {
+    return view('category');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/confirmation', function () {
+    return view('confirmation');
+});
+
+Route::get('/single-product', function () {
+    return view('single-product');
+});
+
+Route::get('/single-blog', function () {
+    return view('single-blog');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/tracking', function () {
+    return view('tracking');
+});
+
+Route::get('/elements', function () {
+    return view('elements');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
